82ac492734effd2f7eeea71f5416b05f5da09a76
chaenii25 <chaeun9325@naver.com>
2026-01-20 14:36:19 +0900

    chore: clear jwt class for rewrite

--- simple-board-backend/src/main/java/com/example/simpleboard/security/JwtProvider.java
+++ simple-board-backend/src/main/java/com/example/simpleboard/security/JwtProvider.java
@@ -1,48 +1,5 @@
 package com.example.simpleboard.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Date;
-
-@Component
 public class JwtProvider {
 
-    private final Key key;
-    private final long accessTokenExpMillis;
-
-    public JwtProvider (
-            @Value("${jwt.secret}") String secret,
-            @Value("${jwt.access-token-exp-min}") long expMin
-    ) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-        this.accessTokenExpMillis = expMin * 60_000L;
-    }
-
-    public String createAccessToken (Long userId, String email) {
-        Date now = new Date();
-        Date exp = new Date(now.getTime() + accessTokenExpMillis);
-
-        return Jwts.builder()
-                .setSubject(String.valueOf(userId))
-                .claim("email",email)
-                .setIssuedAt(now)
-                .setExpiration(exp)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public Jws<Claims> parse (String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token);
-    }
 }
